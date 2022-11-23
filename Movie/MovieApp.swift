be8f28c14dd08559bef95f9ebf945c4353e379af
Robert D'Almeida <96450350+robertdalmeida@users.noreply.github.com>
2022-11-23 13:40:28 +0100

    Initial Commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by Robert D'Almeida on 23/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
