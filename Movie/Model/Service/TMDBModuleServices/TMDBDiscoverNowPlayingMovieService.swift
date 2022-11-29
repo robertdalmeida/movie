e50a86a5bf42abb014495369122497b170e37d28
Robert D'Almeida <96450350+robertdalmeida@users.noreply.github.com>
2022-11-30 00:58:28 +0100

    Fix issue of pagination of now playing

--- Movie/Model/Service/TMDBModuleServices/TMDBDiscoverNowPlayingMovieService.swift
+++ Movie/Model/Service/TMDBModuleServices/TMDBDiscoverNowPlayingMovieService.swift
@@ -9,9 +9,7 @@ final class TMDBDiscoverNowPlayingMovieService: MediaService {
     }
         
     func fetchMedia(page: Int) async throws -> (media:[Media], totalPages: Int?, currentPage: Int?)  {
-        let movieResult = try await configuration.tmdb.discover.movies(sortedBy: .primaryReleaseDate(descending: true), withPeople: nil, page: nil)
+        let movieResult = try await configuration.tmdb.discover.movies(sortedBy: .primaryReleaseDate(descending: true), withPeople: nil, page: page)
         return (try await movieResult.results.asyncMap(adaptor.transform(movie:)), movieResult.totalPages, movieResult.page)
     }
-    
-    
 }
